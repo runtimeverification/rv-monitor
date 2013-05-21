b14a6b776ecfafcda2fa610888ff44746273900d
Traian Florin Serbanuta <traian.serbanuta@gmail.com>
2013-05-21 09:25:43 +0300

    updated batch script.

--- bin/rv-monitor.bat
+++ bin/rv-monitor.bat
@@ -2,6 +2,11 @@
 
 set SRC_ROOT=%~dp0..
 
+if "%1"=="-c" (
+  SHIFT
+  "%SRC_ROOT%\RVC\bin\RVC.bat %*
+)
+
 java -cp "%SRC_ROOT%\lib\rvmonitor.jar;%SRC_ROOT%\lib\logicrepository.jar;%SRC_ROOT%\lib\plugins\*.jar;%SRC_ROOT%\lib\mysql-connector-java-3.0.9-stable-bin.jar" rvmonitor.Main %*
 
 
