24f64d883c8eb5d1603e15a90f11a47ea4e360d6
Traian Florin Serbanuta <traiansf@github.com>
2013-05-23 00:05:50 +0300

    Updated to expand classpath (Java7 win32 bug).

--- RVC/bin/RVC.bat
+++ RVC/bin/RVC.bat
@@ -3,8 +3,12 @@
 set SRC_ROOT=%~dp0..
 
 set PLUGINS=%SRC_ROOT%\lib\plugins
+set CP=%SRC_ROOT%\lib\RVC.jar;%SRC_ROOT%\lib\scala-library.jar
+for /f %%a IN ('dir /b /s "%PLUGINS%\*.jar"') do call :concat %%a
 
-java -cp "%SRC_ROOT%\lib\RVC.jar;%SRC_ROOT%\lib\scala-library.jar;%PLUGINS%\ERE.jar;%PLUGINS%\FSM.jar;%PLUGINS\LTL.jar;%PLUGINS\PTLTL.jar" RVC.Main %*
+java -cp "%CP%" RVC.Main %*
+goto :eof
 
-
-
+:concat
+set CP=%CP%;%1
+goto :eof
--- bin/rv-monitor.bat
+++ bin/rv-monitor.bat
@@ -4,9 +4,19 @@ set SRC_ROOT=%~dp0..
 
 if "%1"=="-c" (
   "%SRC_ROOT%\RVC\bin\RVC.bat" %2
-) else (
-  echo "ala bala"
-)
+) 
+
+set PLUGINS=%SRC_ROOT%\lib\plugins
+set CP=%SRC_ROOT%\lib\rvmonitor.jar;%SRC_ROOT%\lib\logicrepository.jar;%SRC_ROOT%\lib\scala-library.jar;%SRC_ROOT%\lib\mysql-connector-java-3.0.9-stable-bin.jar
+for /f %%a IN ('dir /b /s "%PLUGINS%\*.jar"') do call :concat %%a
+
+java -cp "%CP%"  rvmonitor.Main %*
+goto :eof
+
+:concat
+set CP=%CP%;%1
+goto :eof
+
 
 java -cp "%SRC_ROOT%\lib\rvmonitor.jar;%SRC_ROOT%\lib\logicrepository.jar;%SRC_ROOT%\lib\plugins\*.jar;%SRC_ROOT%\lib\mysql-connector-java-3.0.9-stable-bin.jar" rvmonitor.Main %*
 
