cab7ead27d4c210e8520e2bf4bfbef4cf8bf6916
Traian Florin Serbanuta <traian.serbanuta@gmail.com>
2013-08-22 01:20:10 +0300

    Added tests for six steps.

---
+++ examples/c/safety_critical/six_steps/test_2/six_steps.c
@@ -0,0 +1,25 @@
+#include "../__RVC_SixSteps_Monitor.h"
+
+int main(void) {
+	__RVC_SixSteps_step();
+	__RVC_SixSteps_complete();
+	__RVC_SixSteps_step();
+	__RVC_SixSteps_step();
+	__RVC_SixSteps_step();
+	__RVC_SixSteps_step();
+	__RVC_SixSteps_step();
+	__RVC_SixSteps_complete();
+	__RVC_SixSteps_step();
+	__RVC_SixSteps_step();
+	__RVC_SixSteps_step();
+	__RVC_SixSteps_step();
+	__RVC_SixSteps_step();
+	__RVC_SixSteps_step();
+	__RVC_SixSteps_complete();
+	__RVC_SixSteps_step();
+	__RVC_SixSteps_step();
+	__RVC_SixSteps_step();
+	__RVC_SixSteps_step();
+	__RVC_SixSteps_step();
+	__RVC_SixSteps_complete();
+}
