c4f12045ca2ab134aa013c24fb32ac9d9d0dd5f1
A. Cody Schuffelen <astronomix@gmail.com>
2014-05-31 16:25:07 -0500

    Converted tabs to 4 spaces in ERE test files

--- test/com/runtimeverification/rvmonitor/logicrepository/ere/FSMTest.java
+++ test/com/runtimeverification/rvmonitor/logicrepository/ere/FSMTest.java
@@ -26,68 +26,68 @@ import static org.junit.Assert.*;
  * Test generating deterministic finite automata from extended regular expressions.
  */
 public class FSMTest {
-	
-	private Symbol a;
-	private Symbol b;
-	
-	@Before
-	public void setUp() {
-		a = Symbol.get("a");
-		b = Symbol.get("b");
-	}
-	
-	/**
-	 * Construct the string representation of a DFA matching the given expression.
-	 * @param expr The expression to print the DFA/FSM from.
-	 * @param symbols The symbols present in the expression.
-	 * @return A string representing the DFA/FSM.
-	 */
-	private String printFSM(ERE expr, Symbol... symbols) {
-		FSM dfa = FSM.get(expr, symbols);
-		
-		ByteArrayOutputStream os = new ByteArrayOutputStream();
-		PrintStream ps = new PrintStream(os);
-		dfa.print(ps);
-		
-		return os.toString();
-	}
-	
-	/**
-	 * Test generating the DFA for a Concat element of two Symbols.
-	 */
-	@Test
-	public void testConcat() {
-		ERE ab = Concat.get(a, b);
-		
-		String fsm = "s0 [\n   a -> s1\n]\n" +
-			"s1 [\n   b -> s2\n]\n" +
-			"s2 [\n]\n" +
-			"alias match = s2 \n";
-		assertEquals(fsm, printFSM(ab, a, b));
-	}
-	
-	/**
-	 * Test generating the DFA for an Or element of two Symbols.
-	 */
-	@Test
-	public void testOr() {
-		ERE or = Or.get(new ArrayList<ERE>(Arrays.asList(a, b)));
-		
-		String fsm = "s0 [\n   a -> s1\n   b -> s1\n]\n"+
-			"s1 [\n]\n" +
-			"alias match = s1 \n";
-		assertEquals(fsm, printFSM(or, a, b));
-	}
-	
-	/**
-	 * Test generating the DFA for a Kleene element of one symbol.
-	 */
-	@Test
-	public void testKleene() {
-		ERE aStar = Kleene.get(a);
-		
-		String fsm = "s0 [\n   a -> s0\n]\n" +
-			"alias match = s0 \n";
-		assertEquals(fsm, printFSM(aStar, a));
-	}
+    
+    private Symbol a;
+    private Symbol b;
+    
+    @Before
+    public void setUp() {
+        a = Symbol.get("a");
+        b = Symbol.get("b");
+    }
+    
+    /**
+     * Construct the string representation of a DFA matching the given expression.
+     * @param expr The expression to print the DFA/FSM from.
+     * @param symbols The symbols present in the expression.
+     * @return A string representing the DFA/FSM.
+     */
+    private String printFSM(ERE expr, Symbol... symbols) {
+        FSM dfa = FSM.get(expr, symbols);
+        
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+        PrintStream ps = new PrintStream(os);
+        dfa.print(ps);
+        
+        return os.toString();
+    }
+    
+    /**
+     * Test generating the DFA for a Concat element of two Symbols.
+     */
+    @Test
+    public void testConcat() {
+        ERE ab = Concat.get(a, b);
+        
+        String fsm = "s0 [\n   a -> s1\n]\n" +
+        "s1 [\n   b -> s2\n]\n" +
+        "s2 [\n]\n" +
+        "alias match = s2 \n";
+        assertEquals(fsm, printFSM(ab, a, b));
+    }
+    
+    /**
+     * Test generating the DFA for an Or element of two Symbols.
+     */
+    @Test
+    public void testOr() {
+        ERE or = Or.get(new ArrayList<ERE>(Arrays.asList(a, b)));
+        
+        String fsm = "s0 [\n   a -> s1\n   b -> s1\n]\n"+
+        "s1 [\n]\n" +
+        "alias match = s1 \n";
+        assertEquals(fsm, printFSM(or, a, b));
+    }
+    
+    /**
+     * Test generating the DFA for a Kleene element of one symbol.
+     */
+    @Test
+    public void testKleene() {
+        ERE aStar = Kleene.get(a);
+        
+        String fsm = "s0 [\n   a -> s0\n]\n" +
+        "alias match = s0 \n";
+        assertEquals(fsm, printFSM(aStar, a));
+    }
 }
--- test/com/runtimeverification/rvmonitor/logicrepository/ere/PluginTest.java
+++ test/com/runtimeverification/rvmonitor/logicrepository/ere/PluginTest.java
@@ -15,39 +15,39 @@ import static org.junit.Assert.*;
  * @author A. Cody Schuffelen
  */
 public class PluginTest {
-	
-	/**
-	 * Tests formula generation for ERE code on a simple property.
-	 */
-	@Test
-	public void testCompletePluginHasNext() throws Exception {
-		//based on HasNext at http://fsl.cs.illinois.edu/index.php/Special:EREPlugin3
-		LogicRepositoryType input = new LogicRepositoryType();
-		input.setClient("testing");
-		input.setEvents("hasnext next");
-		input.setCategories("fail");
-		
-		PropertyType property = new PropertyType();
-		property.setLogic("ere");
-		property.setFormula("(hasnext hasnext* next)*");
-		input.setProperty(property);
-		
-		LogicRepositoryType output = PluginHelper.runLogicPlugin("ere", input);
-		
-		assertEquals("testing", output.getClient());
-		assertEquals("hasnext next", output.getEvents());
-		String expectedFormula =
-			"s0 [\n" +
-			"   hasnext -> s1\n" +
-			"]\n" +
-			"s1 [\n" +
-			"   hasnext -> s1\n" +
-			"   next -> s0\n" +
-			"]\n" +
-			"alias match = s0 \n";
-		assertEquals(expectedFormula, output.getProperty().getFormula());
-		assertEquals("fail", output.getCategories());
-		
-	}
-	
+    
+    /**
+     * Tests formula generation for ERE code on a simple property.
+     */
+    @Test
+    public void testCompletePluginHasNext() throws Exception {
+        //based on HasNext at http://fsl.cs.illinois.edu/index.php/Special:EREPlugin3
+        LogicRepositoryType input = new LogicRepositoryType();
+        input.setClient("testing");
+        input.setEvents("hasnext next");
+        input.setCategories("fail");
+        
+        PropertyType property = new PropertyType();
+        property.setLogic("ere");
+        property.setFormula("(hasnext hasnext* next)*");
+        input.setProperty(property);
+        
+        LogicRepositoryType output = PluginHelper.runLogicPlugin("ere", input);
+        
+        assertEquals("testing", output.getClient());
+        assertEquals("hasnext next", output.getEvents());
+        String expectedFormula =
+        "s0 [\n" +
+        "   hasnext -> s1\n" +
+        "]\n" +
+        "s1 [\n" +
+        "   hasnext -> s1\n" +
+        "   next -> s0\n" +
+        "]\n" +
+        "alias match = s0 \n";
+        assertEquals(expectedFormula, output.getProperty().getFormula());
+        assertEquals("fail", output.getCategories());
+        
+    }
+    
 }
