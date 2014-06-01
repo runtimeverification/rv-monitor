52e8a05f0b9f9a0a9bf048a37b4bc6b63be00919
A. Cody Schuffelen <astronomix@gmail.com>
2014-06-01 07:24:40 -0500

    Reformatted Repository ERE to use 4 spaces

--- src/com/runtimeverification/rvmonitor/logicrepository/plugins/ere/Negation.java
+++ src/com/runtimeverification/rvmonitor/logicrepository/plugins/ere/Negation.java
@@ -6,43 +6,43 @@ import java.util.ArrayList;
  * An ERE representing the negation of another ERE.
  */
 public class Negation extends ERE {
-
-  /**
-   * Acquire an instance of a negation of another ERE pattern.
-   * @param child The ERE to negate
-   * @return An ERE that negates the given ERE.
-   */
-  public static ERE get(ERE child){
-	 if(child.getEREType() == EREType.NEG) return child.children.get(0);
-    return new Negation(child);
-  }
-
-  /**
-   * Construct a negation of another ERE.
-   * @param child The ERE to negate.
-   */
-  private Negation(ERE child){
-    children = new ArrayList<ERE>();
-	 children.add(child);
-  }
-
-  public EREType getEREType(){ 
-	  return EREType.NEG;
-  }
-
-  public String toString(){
-	 return "~(" + children.get(0) + ")";
-  }
-
-  public ERE copy(){
-	 return new Negation(children.get(0).copy());
-  }
-
-  public boolean containsEpsilon(){
-	 return !(children.get(0).containsEpsilon());
-  }
-
-  public ERE derive(Symbol s){
-    return Negation.get(children.get(0).derive(s));
-  }
+    
+    /**
+     * Acquire an instance of a negation of another ERE pattern.
+     * @param child The ERE to negate
+     * @return An ERE that negates the given ERE.
+     */
+    public static ERE get(ERE child) {
+        if(child.getEREType() == EREType.NEG) return child.children.get(0);
+        return new Negation(child);
+    }
+    
+    /**
+     * Construct a negation of another ERE.
+     * @param child The ERE to negate.
+     */
+    private Negation(ERE child) {
+        children = new ArrayList<ERE>();
+        children.add(child);
+    }
+    
+    public EREType getEREType() { 
+        return EREType.NEG;
+    }
+    
+    public String toString() {
+        return "~(" + children.get(0) + ")";
+    }
+    
+    public ERE copy() {
+        return new Negation(children.get(0).copy());
+    }
+    
+    public boolean containsEpsilon() {
+        return !(children.get(0).containsEpsilon());
+    }
+    
+    public ERE derive(Symbol s) {
+        return Negation.get(children.get(0).derive(s));
+    }
 }
--- src/com/runtimeverification/rvmonitor/logicrepository/plugins/ere/Repeat.java
+++ src/com/runtimeverification/rvmonitor/logicrepository/plugins/ere/Repeat.java
@@ -5,24 +5,24 @@ package com.runtimeverification.rvmonitor.logicrepository.plugins.ere;
  * An ERE representing another ERE repeated a fixed number of times.
  */
 public class Repeat {
-
-  /**
-   * Acquire an ERE that matches another ERE a fixed number of times.
-   * @param child The ERE to match multiple times.
-   * @param num The number of times to match the child ERE.
-   * @return An ERE matching {@code child @endcode} {@code num @endcode} times.
-   */
-  public static ERE get(ERE child, int num){
-    /* 
-     * We won't even use derive here or even
-     * subclass ERE, we just immediately 
-     * return a new concatenation list of the child repeated
-     * num times.
+    
+    /**
+     * Acquire an ERE that matches another ERE a fixed number of times.
+     * @param child The ERE to match multiple times.
+     * @param num The number of times to match the child ERE.
+     * @return An ERE matching {@code child @endcode} {@code num @endcode} times.
      */
-    ERE ret = Concat.get(child, child);
-    for(int i = 2; i < num; ++i){
-      ret = Concat.get(child, ret); 
-    } 
-    return ret;
-  }
+    public static ERE get(ERE child, int num) {
+        /* 
+         * We won't even use derive here or even
+         * subclass ERE, we just immediately 
+         * return a new concatenation list of the child repeated
+         * num times.
+         */
+        ERE ret = Concat.get(child, child);
+        for(int i = 2; i < num; ++i) {
+            ret = Concat.get(child, ret); 
+        } 
+        return ret;
+    }
 }
