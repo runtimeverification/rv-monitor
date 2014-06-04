1389e65f40f18eec3c4a2652a46cd780d88e9ac6
A. Cody Schuffelen <astronomix@gmail.com>
2014-06-04 14:04:01 -0500

    Reformatted and added documentation to repository FSM

--- src/com/runtimeverification/rvmonitor/logicrepository/plugins/fsm/parser/ast/State.java
+++ src/com/runtimeverification/rvmonitor/logicrepository/plugins/fsm/parser/ast/State.java
@@ -2,28 +2,42 @@ package com.runtimeverification.rvmonitor.logicrepository.plugins.fsm.parser.ast
 
 import java.util.HashMap;
 
+/**
+ * A Finite State Machine state. Accessed using {@link #get(String s)} to limit
+ * the State instances, so they can be compared with == and !=.
+ */
 public class State {
-  private static HashMap<String, State> stringRef;
-  private static HashMap<State, String> refString;
-  
-  static {
-    stringRef = new HashMap<String, State>();
-	 refString = new HashMap<State, String>();
-  }
-
-  public static State get(String s){
-    if(stringRef.containsKey(s)){
-      return stringRef.get(s);
-	 }
-	 else {
-		State ret = new State();
-      stringRef.put(s, ret);
-		refString.put(ret, s);
-		return ret;
-	 }
-  }
-
-  public String toString(){
-    return refString.get(this);
-  }
+    private static HashMap<String, State> stringRef;
+    private static HashMap<State, String> refString;
+    
+    static {
+        stringRef = new HashMap<String, State>();
+        refString = new HashMap<State, String>();
+    }
+    
+    /**
+     * Retrieve the state instance associated with the given name.
+     * @param s The name of the state.
+     * @return The unique state instance with that name.
+     */
+    public static State get(String s) {
+        if(stringRef.containsKey(s)) {
+            return stringRef.get(s);
+        }
+        else {
+            State ret = new State();
+            stringRef.put(s, ret);
+            refString.put(ret, s);
+            return ret;
+        }
+    }
+    
+    /**
+     * The name of the state.
+     * @return The name the state was initialized with.
+     */
+    @Override
+    public String toString() {
+        return refString.get(this);
+    }
 }
