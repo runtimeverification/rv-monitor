588d743ad5cc3f5a17ea5ebe62645fc58a8b1953
A. Cody Schuffelen <astronomix@gmail.com>
2014-06-17 16:16:24 -0500

    Reformatted SRS to use 4 spaces

--- src/com/runtimeverification/rvmonitor/logicrepository/plugins/srs/AbstractSequence.java
+++ src/com/runtimeverification/rvmonitor/logicrepository/plugins/srs/AbstractSequence.java
@@ -3,8 +3,8 @@ package com.runtimeverification.rvmonitor.logicrepository.plugins.srs;
 import java.util.Map;
 
 public interface AbstractSequence { 
-  public String toDotString(); 
-  public int dotLength();
-  public void getImpl(StringBuilder sb, Map<Symbol, Integer> symToNum);
+    public String toDotString(); 
+    public int dotLength();
+    public void getImpl(StringBuilder sb, Map<Symbol, Integer> symToNum);
 }
 
