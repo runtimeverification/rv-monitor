98eea5f9b44a8cf20999980bfe22b42e20e3a661
A. Cody Schuffelen <astronomix@gmail.com>
2014-06-19 15:57:14 -0500

    Reformatted LTL plugin

--- src/com/runtimeverification/rvmonitor/logicrepository/plugins/ltl/DualPreviously.java
+++ src/com/runtimeverification/rvmonitor/logicrepository/plugins/ltl/DualPreviously.java
@@ -6,46 +6,46 @@ import java.util.HashMap;
 
 //class representing an DualPreviously node in an LTL formula
 public class DualPreviously extends LTLFormula {
-
-  public DualPreviously(LTLFormula child){
-    children = new ArrayList<LTLFormula>(1);
-    children.add(child);
-  }
-
-  public LTLType getLTLType(){ 
-     return LTLType.DY;
-  }
-
-  protected LTLFormula normalize(boolean b) {
-    if(b) {
-      return new Previously(
-        new Negation(children.get(0)).normalize(false));
+    
+    public DualPreviously(LTLFormula child){
+        children = new ArrayList<LTLFormula>(1);
+        children.add(child);
     }
-    else{
-      children.set(0,children.get(0).normalize(false));
-      return this;
+    
+    public LTLType getLTLType(){ 
+        return LTLType.DY;
     }
-  }
-
-  public LTLFormula copy(){
-    return new DualPreviously(children.get(0).copy());
-  }
-
- public ATransition d(HashMap<LTLFormula, ATransition> D){
-    LinkedHashSet<ATuple> retTuples 
-   = new LinkedHashSet<ATuple>(1);
-    LinkedHashSet<LTLFormula> empty  
-   = new LinkedHashSet<LTLFormula>(0);
-    LinkedHashSet<LinkedHashSet<LTLFormula>> previousSet
-   = children.get(0).toSetForm();
-    LinkedHashSet<LTLFormula> ENDprevious  
-   = new LinkedHashSet<LTLFormula>(1);
-    ENDprevious.add(END.get());
-
-    for(LinkedHashSet<LTLFormula> previous : previousSet){
-      retTuples.add(new ATuple(previous, sigma, empty));
+    
+    protected LTLFormula normalize(boolean b) {
+        if(b) {
+            return new Previously(
+                new Negation(children.get(0)).normalize(false));
+        }
+        else{
+            children.set(0,children.get(0).normalize(false));
+            return this;
+        }
     }
-    retTuples.add(new ATuple(ENDprevious, sigma, empty)); 
-    return new ATransition(retTuples);
-  }
+    
+    public LTLFormula copy(){
+        return new DualPreviously(children.get(0).copy());
+    }
+    
+    public ATransition d(HashMap<LTLFormula, ATransition> D){
+        LinkedHashSet<ATuple> retTuples 
+        = new LinkedHashSet<ATuple>(1);
+        LinkedHashSet<LTLFormula> empty  
+        = new LinkedHashSet<LTLFormula>(0);
+        LinkedHashSet<LinkedHashSet<LTLFormula>> previousSet
+        = children.get(0).toSetForm();
+        LinkedHashSet<LTLFormula> ENDprevious  
+        = new LinkedHashSet<LTLFormula>(1);
+        ENDprevious.add(END.get());
+        
+        for(LinkedHashSet<LTLFormula> previous : previousSet){
+            retTuples.add(new ATuple(previous, sigma, empty));
+        }
+        retTuples.add(new ATuple(ENDprevious, sigma, empty)); 
+        return new ATransition(retTuples);
+    }
 }
--- src/com/runtimeverification/rvmonitor/logicrepository/plugins/ltl/Next.java
+++ src/com/runtimeverification/rvmonitor/logicrepository/plugins/ltl/Next.java
@@ -6,43 +6,43 @@ import java.util.HashMap;
 
 //class representing an Next node in an LTL formula
 public class Next extends LTLFormula {
-
-  public Next(LTLFormula child){
-    children = new ArrayList<LTLFormula>(1);
-    children.add(child);
-  }
-
-  public LTLType getLTLType(){ 
-     return LTLType.X;
-  }
-
-  protected LTLFormula normalize(boolean b) {
-    if(b) {
-      return new DualNext(
-        new Negation(children.get(0)).normalize(false));
+    
+    public Next(LTLFormula child){
+        children = new ArrayList<LTLFormula>(1);
+        children.add(child);
     }
-    else{
-      children.set(0,children.get(0).normalize(false));
-      return this;
+    
+    public LTLType getLTLType(){ 
+        return LTLType.X;
     }
-  }
-
-  public LTLFormula copy(){
-    return new Next(children.get(0).copy());
-  }
-
-  public ATransition d(HashMap<LTLFormula, ATransition> D){
-    LinkedHashSet<ATuple> retTuples 
-   = new LinkedHashSet<ATuple>(1);
-    LinkedHashSet<LTLFormula> empty  
-   = new LinkedHashSet<LTLFormula>(0);
-    LinkedHashSet<LinkedHashSet<LTLFormula>> nextSet
-   = children.get(0).toSetForm();
-
-    for(LinkedHashSet<LTLFormula> next : nextSet){
-      retTuples.add(new ATuple(empty, sigma, next));
+    
+    protected LTLFormula normalize(boolean b) {
+        if(b) {
+            return new DualNext(
+                new Negation(children.get(0)).normalize(false));
+        }
+        else{
+            children.set(0,children.get(0).normalize(false));
+            return this;
+        }
     }
-    return new ATransition(retTuples);
-  }
-
+    
+    public LTLFormula copy(){
+        return new Next(children.get(0).copy());
+    }
+    
+    public ATransition d(HashMap<LTLFormula, ATransition> D){
+        LinkedHashSet<ATuple> retTuples 
+        = new LinkedHashSet<ATuple>(1);
+        LinkedHashSet<LTLFormula> empty  
+        = new LinkedHashSet<LTLFormula>(0);
+        LinkedHashSet<LinkedHashSet<LTLFormula>> nextSet
+        = children.get(0).toSetForm();
+        
+        for(LinkedHashSet<LTLFormula> next : nextSet){
+            retTuples.add(new ATuple(empty, sigma, next));
+        }
+        return new ATransition(retTuples);
+    }
+    
 }
