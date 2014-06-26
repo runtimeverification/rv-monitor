dd48978d9c5ac6f1238f7ee85f3dd38f7f7d6293
A. Cody Schuffelen <astronomix@gmail.com>
2014-06-26 10:41:48 -0500

    Adding event modifiers to the language-independent parser

--- src/com/runtimeverification/rvmonitor/core/ast/Event.java
+++ src/com/runtimeverification/rvmonitor/core/ast/Event.java
@@ -1,24 +1,39 @@
 package com.runtimeverification.rvmonitor.core.ast;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * An event monitored in the output program.
  */
 public class Event {
     
+    private final List<String> modifiers;
     private final String name;
     private final String definition;
     private final String action;
     
     /**
      * Construct an Event out of its component parts.
+     * @param modifiers Strings that change the meaning of the event.
      * @param name The name of the event to monitor.
      * @param definition The descrption of what the event is on, e.g. its parameters.
      * @param action The action to take on encountering the event.
      */
-    public Event(String name, String definition, String action) {
+    public Event(List<String> modifiers, String name, String definition, String action) {
+        this.modifiers = Collections.unmodifiableList(new ArrayList<String>(modifiers));
         this.name = name;
         this.definition = definition;
         this.action = action;
+    }
+    
+    /**
+     * Strings that affect the meaning of the event.
+     * @return An unmodifiable list of strings.
+     */
+    public List<String> getModifiers() {
+        return modifiers;
     }
     
     /**
