aa8ae270b781e59faf6ca0abfdcdc145eef17c1f
A. Cody Schuffelen <astronomix@gmail.com>
2014-06-27 14:04:53 -0500

    Print full stack trace on violation, with the correct name

--- src/com/runtimeverification/rvmonitor/java/rvj/output/Util.java
+++ src/com/runtimeverification/rvmonitor/java/rvj/output/Util.java
@@ -4,8 +4,9 @@ import com.runtimeverification.rvmonitor.java.rvj.parser.ast.PackageDeclaration;
 
 public class Util {
 
-	public static final String defaultLocation = "Thread.currentThread()" +
-			".getStackTrace()[4].toString()";
+    public static final String defaultLocation = "System.getProperty(\"line.separator\") + " +
+        "Arrays.toString(new Exception().getStackTrace()).replace(\", \", System.getProperty(\"line.separator\"))"
+        + " + System.getProperty(\"line.separator\")";
 
   public static String packageAndNameToUrl(PackageDeclaration packageDeclaration, String name){
     return "http://fsl.cs.uiuc.edu/annotated-java/__properties/html/" +
