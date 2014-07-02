919089766f47ab9acacb1f9003affc6cc1940f42
A. Cody Schuffelen <astronomix@gmail.com>
2014-07-02 15:00:45 -0500

    Renamed rt.jar to rvmonitorrt.jar

--- bin/rv-monitor.bat
+++ bin/rv-monitor.bat
@@ -10,7 +10,7 @@ if "%1"=="-c" (
 ) 
 
 set PLUGINS=%SRC_ROOT%\lib\plugins
-set CP=%SRC_ROOT%\lib\rvmonitor.jar;%SRC_ROOT%\lib\logicrepository.jar;%SRC_ROOT%\lib\scala-library.jar;%SRC_ROOT%\lib\mysql-connector-java-3.0.9-stable-bin.jar
+set CP=%SRC_ROOT%\lib\rvmonitorrt.jar;%SRC_ROOT%\lib\rvmonitor.jar;%SRC_ROOT%\lib\logicrepository.jar;%SRC_ROOT%\lib\scala-library.jar;%SRC_ROOT%\lib\mysql-connector-java-3.0.9-stable-bin.jar
 for /f %%a IN ('dir /b /s "%PLUGINS%\*.jar"') do call :concat %%a
 
 java -cp "%CP%"  %MAINCLASS% %1 %2 %3 %4 %5 %6 %7 %8 %9
