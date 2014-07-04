0406079638f8d26da9b30f1f72fff71621461584
willschellhornrv <will.schellhorn@runtimeverification.com>
2014-07-04 09:32:07 -0500

    fix AroundAdvice (casino)

--- examples/java/MOPSyntax/AroundAdvice/CheatedSlotMachine_1/CheatedSlotMachine_1.java
+++ examples/java/MOPSyntax/AroundAdvice/CheatedSlotMachine_1/CheatedSlotMachine_1.java
@@ -1,6 +1,6 @@
 package CheatedSlotMachine_1;
 
-import mop.CheatedSlotMachineRuntimeMonitor;
+import rvm.CheatedSlotMachineRuntimeMonitor;
 
 import casino.SlotMachine;
 
@@ -10,10 +10,10 @@ public class CheatedSlotMachine_1 {
 		for (int i = 0; i < 10 ; ++i) {
 			System.out.println("Round " + i);
 			machine.insertCoin();
-			mop.CheatedSlotMachineRuntimeMonitor.insert_coinEvent(machine);
+			rvm.CheatedSlotMachineRuntimeMonitor.insert_coinEvent(machine);
 			machine.push();
-			mop.CheatedSlotMachineRuntimeMonitor.push_buttonEvent(machine);
-			mop.CheatedSlotMachineRuntimeMonitor.resultEvent(machine);
+			rvm.CheatedSlotMachineRuntimeMonitor.push_buttonEvent(machine);
+			rvm.CheatedSlotMachineRuntimeMonitor.resultEvent(machine);
 			if (!CheatedSlotMachineRuntimeMonitor.skipEvent)
 				System.out.println(machine.getResult());
 		}
