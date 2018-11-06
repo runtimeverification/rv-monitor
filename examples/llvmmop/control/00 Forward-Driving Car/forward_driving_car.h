c709b9a1f0e5b22d4ba85c22b53c1f2b9132a106
Brandon Moore <brandon.moore@runtimeverification.com>
2018-11-06 17:37:02 -0600

    Instrumenting KeYmaera-generated code

---
+++ examples/llvmmop/control/00 Forward-Driving Car/forward_driving_car.h
@@ -0,0 +1,17 @@
+﻿#ifndef FORWARD_DRIVING_CAR_H
+#define FORWARD_DRIVING_CAR_H
+
+typedef struct parameters {
+  long double A; // maximum acceleration
+  long double B; // maximum braking
+} parameters;
+
+typedef struct state {
+  long double a;
+  long double v;
+  long double x;
+} state;
+
+extern state ctrlStep(state curr, const parameters* const params);
+
+#endif
