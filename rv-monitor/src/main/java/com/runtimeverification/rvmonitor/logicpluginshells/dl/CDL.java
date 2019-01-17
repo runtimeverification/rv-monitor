066d568a153e25c674314bf67fca1c2ec359c3c3
Manasvi Saxena <msaxena2@illinois.edu>
2019-01-16 16:50:26 -0800

    logicpluginshells/dl/CDL.java : C monitor synthesis for dL

--- rv-monitor/src/main/java/com/runtimeverification/rvmonitor/logicpluginshells/dl/CDL.java
+++ rv-monitor/src/main/java/com/runtimeverification/rvmonitor/logicpluginshells/dl/CDL.java
@@ -10,6 +10,7 @@ import java.util.Properties;
 import java.util.Set;
 
 import com.runtimeverification.rvmonitor.c.rvc.CSpecification;
+import com.runtimeverification.rvmonitor.core.ast.Event;
 import com.runtimeverification.rvmonitor.logicpluginshells.LogicPluginShell;
 import com.runtimeverification.rvmonitor.logicpluginshells.LogicPluginShellResult;
 import com.runtimeverification.rvmonitor.logicpluginshells.fsm.ast.FSMAlias;
@@ -34,7 +35,7 @@ public class CDL extends LogicPluginShell {
 
     public CDL(CSpecification cSpec, boolean parametric) {
         super();
-        monitorType = "CDL";
+        monitorType = "DL";
         outputLanguage = "C";
         this.cSpec = cSpec;
         this.parametric = parametric;
@@ -49,385 +50,56 @@ public class CDL extends LogicPluginShell {
         }
     }
 
-    private Properties getNonParametricMonitorCode(
-            LogicRepositoryType logicOutput) throws RVMException {
-//        String rvcPrefix = "__RVC_";
-//        Properties result = new Properties();
-//
-//        String monitor = logicOutput.getProperty().getFormula();
-//        String specName = logicOutput.getSpecName() + "_";
-//        String constSpecName = specName.toUpperCase();
-//
-//        result.setProperty("rvcPrefix", rvcPrefix);
-//        result.setProperty("specName", specName);
-//        result.setProperty("constSpecName", constSpecName);
-//
-//        FSMInput fsmInput = null;
-//        try {
-//            fsmInput = FSMParser.parse(new ByteArrayInputStream(monitor
-//                    .getBytes()));
-//        } catch (Exception e) {
-//            System.out.println(e.getMessage());
-//            throw new RVMException(
-//                    "FSM to C LogicPluginShell cannot parse FSM formula");
-//        }
-//
-//        if (fsmInput == null)
-//            throw new RVMException(
-//                    "FSM to C LogicPluginShell cannot parse FSM formula");
-//
-//        List<String> monitoredEvents;
-//        Map<String, String> eventArrays = new HashMap<String, String>(
-//                allEvents.size());
-//        monitoredEvents = allEvents;
-//
-//        Map<String, Integer> EventNum = new HashMap<String, Integer>();
-//        int countEvent = 1;
-//
-//        Map<String, String> constEventNames = new HashMap<String, String>(
-//                allEvents.size());
-//
-//        for (String event : monitoredEvents) {
-//            EventNum.put(event, new Integer(countEvent));
-//            String constEventName = rvcPrefix + constSpecName
-//                    + event.toUpperCase();
-//            eventArrays.put(event, "static int " + constEventName + "[] = {");
-//            constEventNames.put(event, constEventName);
-//        }
-//
-//        Map<String, Integer> stateNum = new HashMap<String, Integer>();
-//        int countState = 0;
-//
-//        if (fsmInput.getItems() != null) {
-//            for (FSMItem i : fsmInput.getItems()) {
-//                String stateName = i.getState();
-//                stateNum.put(stateName, new Integer(countState));
-//                countState++;
-//            }
-//        }
-//
-//        result.setProperty("reset", "void\n" + rvcPrefix + specName
-//                + "reset(void)\n{\n  __RVC_state = 0;\n }\n");
-//
-//        for (FSMItem i : fsmInput.getItems()) {
-//            Set<String> unseenEvents = new HashSet<String>(monitoredEvents);
-//            int defaultState = -1;
-//            for (FSMTransition t : i.getTransitions()) {
-//                if (t.isDefaultFlag()) {
-//                    defaultState = stateNum.get(t.getStateName());
-//                } else {
-//                    String eventName = t.getEventName();
-//                    unseenEvents.remove(eventName);
-//                    String arrayString = eventArrays.get(eventName);
-//                    eventArrays
-//                            .put(eventName,
-//                                    arrayString
-//                                            + stateNum.get(t.getStateName())
-//                                            + ", ");
-//                }
-//            }
-//            for (String event : unseenEvents) {
-//                String arrayString = eventArrays.get(event);
-//                eventArrays.put(event, arrayString + defaultState + ",");
-//            }
-//        }
-//
-//        for (String event : monitoredEvents) {
-//            String arrayString = eventArrays.get(event);
-//            eventArrays.put(event, arrayString + "};\n");
-//        }
-//
-//        String monitoringbodyString = "";
-//
-//        for (String eventArray : eventArrays.values()) {
-//            monitoringbodyString += eventArray;
-//        }
-//
-//        addStateDeclaration(result);
-//
-//        // figure out which categories we actually care about
-//        HashSet<String> cats = new HashSet<String>();
-//        for (String category : logicOutput.getCategories().split("\\s+")) {
-//            cats.add(category);
-//        }
-//        String catString = "";
-//
-//        String condString = "";
-//        // add fail if necessary
-//        if (cats.contains("fail")) {
-//            catString = "int " + rvcPrefix + specName + "fail = 0;\n";
-//            condString += "  " + rvcPrefix + specName
-//                    + "fail = __RVC_state == -1;\n";
-//        }
-//
-//        for (String stateName : stateNum.keySet()) {
-//            if (!cats.contains(stateName))
-//                continue;
-//            String catName = rvcPrefix + specName + stateName;
-//            condString += "  " + catName + " = __RVC_state == "
-//                    + stateNum.get(stateName) + ";\n";
-//            catString += "int " + catName + " = 0;\n";
-//        }
-//
-//        if (fsmInput.getAliases() != null) {
-//            for (FSMAlias a : fsmInput.getAliases()) {
-//                String stateName = a.getGroupName();
-//                if (!cats.contains(stateName))
-//                    continue;
-//                String conditionStr = "";
-//                boolean firstFlag = true;
-//                for (String state : a.getStates()) {
-//                    if (firstFlag) {
-//                        conditionStr += "__RVC_state == " + stateNum.get(state);
-//                        firstFlag = false;
-//                    } else {
-//                        conditionStr += " || __RVC_state == "
-//                                + stateNum.get(state);
-//                    }
-//                }
-//                String catName = rvcPrefix + specName + stateName;
-//                condString += "  " + catName + " = " + conditionStr + ";\n";
-//                catString += "int " + catName + " = 0;\n";
-//            }
-//        }
-//
-//        // TODO: fixme
-//        if (catString.equals("")) {
-//            throw new RuntimeException(
-//                    "Invalid handlers specified.  Make sure your logic supports your specified handlers");
-//        }
-//
-//        StringBuilder headerDecs = new StringBuilder();
-//        StringBuilder eventFuncs = new StringBuilder();
-//
-//        String resetName = rvcPrefix + specName + "reset";
-//
-//        headerDecs.append("void\n");
-//        headerDecs.append(resetName + "(void);\n");
-//
-//        for (String eventName : monitoredEvents) {
-//            headerDecs.append("void\n");
-//            eventFuncs.append("void\n");
-//            String funcDecl = rvcPrefix + specName + eventName
-//                    + cSpec.getParameters().get(eventName);
-//            headerDecs.append(funcDecl + ";\n");
-//            eventFuncs.append(funcDecl + "\n");
-//            eventFuncs.append("{\n");
-//            eventFuncs.append(cSpec.getEvents().get(eventName).getAction() + "\n");
-//            eventFuncs.append("__RVC_state = " + constEventNames.get(eventName)
-//                    + "[__RVC_state];\n");
-//            eventFuncs.append(condString);
-//            for (String category : cSpec.getHandlers().keySet()) {
-//                eventFuncs.append("if(" + rvcPrefix + specName + category
-//                        + ")\n{\n");
-//                eventFuncs.append(cSpec.getHandlers().get(category)
-//                        .replaceAll("__RESET", resetName + "()\n"));
-//                eventFuncs.append("}\n");
-//            }
-//            eventFuncs.append("}\n\n");
-//        }
-//
-//        result.setProperty("header declarations", headerDecs.toString());
-//        result.setProperty("event functions", eventFuncs.toString());
-//        result.setProperty("monitoring body", monitoringbodyString);
-//
-//        result.setProperty("categories", catString);
-//
-//        return result;
-        return null;
+    private String modelMonitorFromModelPlex(String keymaeraFile) {
+        return "void monitorSatisfied();";
+    }
+
+    private String toCFunctionDecl(String fName, Event fEvent) {
+        return "void " + fName + "(" + fEvent.getDefinition() + ")";
+    }
+
+    private String toCFunction(String fName, Event fEvent) {
+        StringBuffer functionDef = new StringBuffer();
+        functionDef.append(toCFunctionDecl(fName, fEvent) + "{ \n");
+        functionDef.append("\t" + fEvent.getAction() + "\n");
+        functionDef.append("}\n");
+        return functionDef.toString();
+    }
+
+    private Properties getNonParametricMonitorCode(LogicRepositoryType logicOutput) throws RVMException {
+        String rvcPrefix = "__RVC_";
+        Properties result = new Properties();
+
+        String specName = logicOutput.getSpecName() + "_";
+        String constSpecName = specName.toUpperCase();
+
+        result.setProperty("rvcPrefix", rvcPrefix);
+        result.setProperty("specName", specName);
+        result.setProperty("constSpecName", constSpecName);
+
+        StringBuilder headerDecs = new StringBuilder();
+        StringBuilder eventFuncs = new StringBuilder();
+
+
+        String[] monitoredEvents = logicOutput.getEvents().trim().split("\\s+");
+
+        for (String eventName : monitoredEvents) {
+            Event event = cSpec.getEvents().get(eventName);
+            String funcName = rvcPrefix + specName + eventName;
+            headerDecs.append(toCFunctionDecl(funcName, event) + ";\n");
+            eventFuncs.append(toCFunction(funcName, event));
+        }
+
+        result.setProperty("header declarations", headerDecs.toString());
+        result.setProperty("event functions", eventFuncs.toString());
+        result.setProperty("monitoring body", modelMonitorFromModelPlex(logicOutput.getProperty().getFormula()));
+
+        return result;
     }
 
     private Properties getParametricMonitorCode(LogicRepositoryType logicOutput)
             throws RVMException {
-//        String rvcPrefix = "__RVC_";
-//        Properties result = new Properties();
-//
-//        String monitor = logicOutput.getProperty().getFormula();
-//        String specName = logicOutput.getSpecName() + "_";
-//        String constSpecName = specName.toUpperCase();
-//
-//        result.setProperty("rvcPrefix", rvcPrefix);
-//        result.setProperty("specName", specName);
-//        result.setProperty("constSpecName", constSpecName);
-//
-//        FSMInput fsmInput = null;
-//        try {
-//            fsmInput = FSMParser.parse(new ByteArrayInputStream(monitor
-//                    .getBytes()));
-//        } catch (Exception e) {
-//            System.out.println(e.getMessage());
-//            throw new RVMException(
-//                    "FSM to C LogicPluginShell cannot parse FSM formula");
-//        }
-//
-//        if (fsmInput == null)
-//            throw new RVMException(
-//                    "FSM to C LogicPluginShell cannot parse FSM formula");
-//
-//        List<String> monitoredEvents;
-//        Map<String, String> eventArrays = new HashMap<String, String>(
-//                allEvents.size());
-//        monitoredEvents = allEvents;
-//
-//        Map<String, Integer> EventNum = new HashMap<String, Integer>();
-//        int countEvent = 1;
-//
-//        Map<String, String> constEventNames = new HashMap<String, String>(
-//                allEvents.size());
-//
-//        for (String event : monitoredEvents) {
-//            EventNum.put(event, new Integer(countEvent));
-//            String constEventName = rvcPrefix + constSpecName
-//                    + event.toUpperCase();
-//            eventArrays.put(event, "static int " + constEventName + "[] = {");
-//            constEventNames.put(event, constEventName);
-//        }
-//
-//        Map<String, Integer> stateNum = new HashMap<String, Integer>();
-//        int countState = 0;
-//
-//        if (fsmInput.getItems() != null) {
-//            for (FSMItem i : fsmInput.getItems()) {
-//                String stateName = i.getState();
-//                stateNum.put(stateName, new Integer(countState));
-//                countState++;
-//            }
-//        }
-//
-//        result.setProperty("reset", "void\n"
-//                + rvcPrefix
-//                + specName
-//                + "reset(void *key)\n{\n__RV_monitor * temp = __RV_find(list,key);\nif(temp != NULL) {\ntemp->__RVC_state = 0;}\n }\n");
-//
-//        for (FSMItem i : fsmInput.getItems()) {
-//            Set<String> unseenEvents = new HashSet<String>(monitoredEvents);
-//            int defaultState = -1;
-//            for (FSMTransition t : i.getTransitions()) {
-//                if (t.isDefaultFlag()) {
-//                    defaultState = stateNum.get(t.getStateName());
-//                } else {
-//                    String eventName = t.getEventName();
-//                    unseenEvents.remove(eventName);
-//                    String arrayString = eventArrays.get(eventName);
-//                    eventArrays
-//                            .put(eventName,
-//                                    arrayString
-//                                            + stateNum.get(t.getStateName())
-//                                            + ", ");
-//                }
-//            }
-//            for (String event : unseenEvents) {
-//                String arrayString = eventArrays.get(event);
-//                eventArrays.put(event, arrayString + defaultState + ",");
-//            }
-//        }
-//
-//        for (String event : monitoredEvents) {
-//            String arrayString = eventArrays.get(event);
-//            eventArrays.put(event, arrayString + "};\n");
-//        }
-//
-//        String monitoringbodyString = "";
-//
-//        for (String eventArray : eventArrays.values()) {
-//            monitoringbodyString += eventArray;
-//        }
-//
-//        // figure out which categories we actually care about
-//        HashSet<String> cats = new HashSet<String>();
-//        for (String category : logicOutput.getCategories().split("\\s+")) {
-//            cats.add(category);
-//        }
-//
-//        addParametricStateDeclaration(result, cats);
-//        String catString = "";
-//
-//        String condString = "";
-//        // add fail if necessary
-//        if (cats.contains("fail")) {
-//            condString += "  mon->" + rvcPrefix + specName
-//                    + "fail = mon->__RVC_state == -1;\n";
-//        }
-//
-//        for (String stateName : stateNum.keySet()) {
-//            if (!cats.contains(stateName))
-//                continue;
-//            String catName = rvcPrefix + specName + stateName;
-//            condString += "  mon->" + catName + " = mon->__RVC_state == "
-//                    + stateNum.get(stateName) + ";\n";
-//        }
-//
-//        if (fsmInput.getAliases() != null) {
-//            for (FSMAlias a : fsmInput.getAliases()) {
-//                String stateName = a.getGroupName();
-//                if (!cats.contains(stateName))
-//                    continue;
-//                String conditionStr = "";
-//                boolean firstFlag = true;
-//                for (String state : a.getStates()) {
-//                    if (firstFlag) {
-//                        conditionStr += "mon->__RVC_state == "
-//                                + stateNum.get(state);
-//                        firstFlag = false;
-//                    } else {
-//                        conditionStr += " || mon->__RVC_state == "
-//                                + stateNum.get(state);
-//                    }
-//                }
-//                String catName = rvcPrefix + specName + stateName;
-//                condString += "  mon->" + catName + " = " + conditionStr
-//                        + ";\n";
-//            }
-//        }
-//
-//        StringBuilder headerDecs = new StringBuilder();
-//        StringBuilder eventFuncs = new StringBuilder();
-//
-//        String resetName = rvcPrefix + specName + "reset";
-//
-//        headerDecs.append("void\n");
-//        headerDecs.append(resetName + "(void *key);\n");
-//
-//        for (String eventName : monitoredEvents) {
-//            headerDecs.append("void\n");
-//            eventFuncs.append("void\n");
-//            String funcDecl = rvcPrefix + specName + eventName
-//                    + cSpec.getPParameters().get(eventName);
-//            headerDecs.append(funcDecl + ";\n");
-//            eventFuncs.append(funcDecl + "\n");
-//            eventFuncs.append("{\n");
-//            eventFuncs.append(cSpec.getEvents().get(eventName).getAction() + "\n");
-//            eventFuncs.append("if(list == NULL){\n");
-//            eventFuncs.append("  list = __RV_new_RV_list(10);\n");
-//            eventFuncs.append("}\n");
-//            eventFuncs.append("__RV_monitor *mon = __RV_find(list, key);\n");
-//            eventFuncs.append("if(mon == NULL){\n");
-//            eventFuncs
-//                    .append("  __RV_tag_monitor *tm = __RV_new_RV_tag_monitor(key,0);\n");
-//            eventFuncs.append("  mon = tm->monitor;\n");
-//            eventFuncs.append("  __RV_append(list,tm);\n");
-//            eventFuncs.append("}\n");
-//            eventFuncs.append("mon->__RVC_state = "
-//                    + constEventNames.get(eventName) + "[mon->__RVC_state];\n");
-//            eventFuncs.append(condString);
-//            for (String category : cSpec.getHandlers().keySet()) {
-//                eventFuncs.append("if(mon->" + rvcPrefix + specName + category
-//                        + ")\n{\n");
-//                eventFuncs.append(cSpec.getHandlers().get(category)
-//                        .replaceAll("__RESET", resetName + "()\n"));
-//                eventFuncs.append("}\n");
-//            }
-//            eventFuncs.append("}\n\n");
-//        }
-//
-//        result.setProperty("header declarations", headerDecs.toString());
-//        result.setProperty("event functions", eventFuncs.toString());
-//        result.setProperty("monitoring body", monitoringbodyString);
-//
-//        result.setProperty("categories", catString);
-//
-//        return result;
-        return null;
+        throw new RVMException("Parametric DL monitoring not supported");
     }
 
 
@@ -441,12 +113,7 @@ public class CDL extends LogicPluginShell {
                     "Wrong type of monitor is given to DL Monitor.");
 
         LogicPluginShellResult logicShellResult = new LogicPluginShellResult();
-//        logicShellResult.startEvents = getEvents(logicOutputXML
-//                .getCreationEvents());
-//        // logicShellResult.startEvents = allEvents;
-//        logicShellResult.properties = getMonitorCode(logicOutputXML);
-//        logicShellResult.properties = addEnableSets(
-//                logicShellResult.properties, logicOutputXML);
+        logicShellResult.properties = getMonitorCode(logicOutputXML);
 
         return logicShellResult;
     }
