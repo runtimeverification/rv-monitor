6a53d129766daa094d1e5fcf046c86e697d21c05
Manasvi Saxena <msaxena2@illinois.edu>
2019-01-22 09:37:32 -0800

    dl/DLPlugin.java: Remove unneccessary comment for constructor

--- plugins_logicrepository/dl/src/main/java/com/runtimeverification/rvmonitor/logicrepository/plugins/dl/DLPlugin.java
+++ plugins_logicrepository/dl/src/main/java/com/runtimeverification/rvmonitor/logicrepository/plugins/dl/DLPlugin.java
@@ -4,17 +4,12 @@ import com.runtimeverification.rvmonitor.logicrepository.parser.logicrepositorys
 import com.runtimeverification.rvmonitor.logicrepository.plugins.LogicPlugin;
 
 public class DLPlugin extends LogicPlugin {
-    
-    /**
-     * Apply the Finite State Machine plugin to some input.
-     * @param logicInputXML The bare Finite State Machine input.
-     * @return The annotated and minimized Finite State Machine output.
-     */
+
     public LogicRepositoryType process(LogicRepositoryType logicInputXML) {
         LogicRepositoryType logicOutputXML = logicInputXML;
         logicOutputXML.getMessage().add("done");
 
         return logicOutputXML;
     }
-    
+
 }
